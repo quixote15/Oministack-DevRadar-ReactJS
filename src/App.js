1ded3cfa493415dcd3500097f4f546626e01a541
Tiago <tiago.santos@ayoapp.com.br>
2020-01-27 15:58:59 -0300

    Cria layout do formulario

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './global.css'
+import './aside.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <labal htmlFor="github_username">Usuário do Github</labal>
+            <input name="github_username" id="github_username" required/>
+          </div>
+
+          <div class="input-block">
+            <labal htmlFor="">Tecnologias</labal>
+            <input name="techs" id="techs" required/>
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <labal htmlFor="">Latitude</labal>
+              <input name="latitude" id="latitude" required/>
+            </div>
+
+            <div class="input-block">
+              <labal htmlFor="">Longitude</labal>
+              <input name="longitude" id="longitude" required/>
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+
+         
+        </form>
+      </aside>
+      <main></main>
     </div>
   );
 }
